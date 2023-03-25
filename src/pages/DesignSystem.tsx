1df41eaf715262feca107619eb0533416a00d24a
Emeric LAMBERT <emericlambert08@gmail.com>
2023-03-25 17:19:00 +0100

    style(tabs): tabs component with animation

--- src/pages/DesignSystem.tsx
+++ src/pages/DesignSystem.tsx
@@ -4,6 +4,7 @@ import Select from "@/components/01 - Atoms/Select/Select";
 import Multiselect from "@/components/03 - Organisms/Multiselect/Multiselect";
 import useToasts from "@/hooks/useToasts";
 import Dialog from "@/components/04 - Templates/Dialog/Dialog";
+import Tabs from "@/components/03 - Organisms/Tabs/Tabs";
 import { useState } from "react";
 
 export default function DesignSystem() {
@@ -28,6 +29,30 @@ export default function DesignSystem() {
         visible={dialog}
         Dismiss={() => setDialog(!dialog)}
       >
+
+        <Tabs tabs={[
+          {
+            tabTitle: 'test',
+            tabContent:
+              <Button theme="primary">
+                bro la meuf la jla baise
+              </Button>
+          },
+          {
+            tabTitle: 'test2',
+            tabContent:
+              <Button theme="primary">
+                piou piou
+              </Button>
+          },
+          {
+            tabTitle: 'caca',
+            tabContent:
+              <Button theme="primary">
+                j'adore jouer avec mon caca
+              </Button>
+          }
+        ]} />
 
       </Dialog>
 
