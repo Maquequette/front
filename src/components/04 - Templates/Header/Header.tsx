95f334cbdb5c9045c75999dd788a01cbcd0bd31b
BeatlesBOB <beatlesbob74120@gmail.com>
2023-03-26 22:53:23 +0200

    feat: 🎸 ajout du hook pour la gestion du scroll

--- src/components/04 - Templates/Header/Header.tsx
+++ src/components/04 - Templates/Header/Header.tsx
@@ -4,13 +4,20 @@ import Container from "@/components/01 - Atoms/Container/Container";
 import Navigation from "@/components/03 - Organisms/Navigation/Navigation";
 import Logo from "@/components/01 - Atoms/Logo/Logo";
 import Burger from "@/components/02 -  Molecules/Burger/Burger";
+import useDisableScroll from "@/hooks/useDisableScroll";
 import "./Header.scss";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const { enable, disable } = useDisableScroll();
 
   const toggleOpen = () => {
-    setIsOpen((prev) => !prev);
+    setIsOpen(!isOpen);
+    if (!isOpen) {
+      disable();
+    } else {
+      enable();
+    }
   };
 
   return (
