1888e88ad7581e61a5ea467dcbee6f1b7586b6fe
BeatlesBOB <beatlesbob74120@gmail.com>
2023-04-06 21:34:19 +0200

    fix: 🐛 dialog open

--- src/components/04 - Templates/Dialog/Dialog.tsx
+++ src/components/04 - Templates/Dialog/Dialog.tsx
@@ -1,41 +1,26 @@
-import React, { MouseEventHandler, CSSProperties, ReactNode } from 'react'
-import "./Dialog.scss"
+import React, { MouseEventHandler, CSSProperties, ReactNode } from "react";
+import "./Dialog.scss";
 
 export interface IDialog {
-  id: string
-  children: ReactNode
-  visible: boolean
-  Dismiss: Function
-  styles?: CSSProperties
+  id: string;
+  children: ReactNode;
+  visible: boolean;
+  Dismiss: Function;
+  styles?: CSSProperties;
 }
 
-export default function Dialog({
-  id,
-  children,
-  visible = false,
-  Dismiss,
-  styles
-}: IDialog) {
-
+export default function Dialog({ id, children, visible = false, Dismiss, styles }: IDialog) {
   const backdropCloseModal = (e: React.MouseEvent<HTMLDialogElement>) => {
-    Dismiss()
-  }
+    Dismiss();
+  };
 
   return (
-    <dialog
-      className="dialog"
-      id={id}
-      open={visible}
-      onClick={backdropCloseModal}
-    >
-
+    <dialog className="dialog" id={id} open={visible} onClick={backdropCloseModal}>
       <div
-        className='dialog__content'
-        onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}
-      >
+        className="dialog__content"
+        onClick={(e: React.MouseEvent<HTMLDivElement>) => e.stopPropagation()}>
         {children}
       </div>
-
     </dialog>
-  )
+  );
 }
