33563f69f9b5c6772ee9b424054edb02e2a72c95
Agence-de-Develoloppeurweb <develoloppeurweb@gmail.com>
2023-04-08 14:35:24 +0200

    refactor(rename classroom): rename ClassRoom

--- src/pages/ClassRoom.tsx
+++ src/pages/ClassRoom.tsx
@@ -1,5 +1,5 @@
 import PageTransition from "@/components/04 - Templates/PageTransition/PageTransition";
 
-export default function ClassRoom() {
-  return <PageTransition>ClassRoom</PageTransition>;
+export default function Classroom() {
+  return <PageTransition>Classroom</PageTransition>;
 }
