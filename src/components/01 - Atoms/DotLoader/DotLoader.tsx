3f5ff6ef3762c025cb213e9551a55640b2aa8b53
BeatlesBOB <beatlesbob74120@gmail.com>
2023-05-14 21:27:45 +0200

    fix: 🐛 correction dot loader

--- src/components/01 - Atoms/DotLoader/DotLoader.tsx
+++ src/components/01 - Atoms/DotLoader/DotLoader.tsx
@@ -1,5 +1,12 @@
+import { Theme } from "@/types/Theme";
 import "./DotLoader.scss";
 
-export default function DotLoader() {
-  return <span className="dotLoader"></span>;
+export default function DotLoader({ theme }: { theme: Theme }) {
+  return (
+    <div className={`dotLoader dotLoader--${theme}`}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
 }
--- src/pages/DesignSystem.tsx
+++ src/pages/DesignSystem.tsx
@@ -1,10 +1,5 @@
 import DotLoader from "@/components/01 - Atoms/DotLoader/DotLoader";
-import Spinner from "@/components/01 - Atoms/Spinner/Spinner";
+
 export default function DesignSystem() {
-  return (
-    <>
-      <DotLoader />
-      <Spinner theme="primary" />
-    </>
-  );
+  return <DotLoader theme="secondary" />;
 }
