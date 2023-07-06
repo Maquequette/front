18fa5fcccc7466ce0c71c74dfefc7b3f39b523a4
BeatlesBOB <beatlesbob74120@gmail.com>
2023-07-06 19:36:59 +0200

    feat: 🎸 fix navigation badg

--- src/components/01 - Atoms/Badge/Badge.tsx
+++ src/components/01 - Atoms/Badge/Badge.tsx
@@ -2,15 +2,17 @@ import { ReactNode, useState, memo } from "react";
 import clsx from "clsx";
 import useClickOutside from "@/hooks/useClickOutside";
 import "./Badge.scss";
+import { Theme } from "@/types/Theme";
 
 export interface IBadge {
   content?: string;
   children: ReactNode;
   id?: string;
-  color: any;
+  color?: any;
+  theme?: Theme;
 }
 
-export default memo(function Badge({ content, children, id, color }: IBadge) {
+export default memo(function Badge({ content, children, id, color, theme }: IBadge) {
   const [visible, setVisibility] = useState<boolean>(false);
   const ref = useClickOutside(() => setVisibility(false));
 
@@ -18,7 +20,7 @@ export default memo(function Badge({ content, children, id, color }: IBadge) {
     <div className="badge" id={id} ref={ref}>
       <button
         type="button"
-        className={`badge__btn`}
+        className={`badge__btn ${theme ? "badge__btn--" + theme : ""}`}
         style={{ backgroundColor: `${color?.code}` }}
         onClick={() => setVisibility(!visible)}>
         {content}
