89399ba549f5d6d699abcab71cee7bffec3ee8ee
BeatlesBOB <beatlesbob74120@gmail.com>
2023-07-18 08:53:21 +0200

    Update Like.tsx

--- src/components/02 - Molecules/Like/Like.tsx
+++ src/components/02 - Molecules/Like/Like.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useReducer, useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import Svg from "@/components/01 - Atoms/Svg/Svg";
+import { AuthContext } from "@/contexts/AuthContext";
 import { likeChallenge, unlikeChallenge } from "@/services/challenges.service";
 import "./Like.scss";
 
@@ -16,7 +17,8 @@ export default function Like({ id, isAlreadyLiked, likesCount = 0, showNumber }:
   const [count, setCount] = useState(likesCount);
   const { mutate: like } = useMutation(likeChallenge);
   const { mutate: unlike } = useMutation(unlikeChallenge);
-
+  const { isConnected } = useContext(AuthContext);
+  
   const handleLike = useCallback(() => {
     isLiked ? unlike(id) : like(id);
     setIsLiked(!isLiked);
@@ -24,9 +26,9 @@ export default function Like({ id, isAlreadyLiked, likesCount = 0, showNumber }:
   }, [isLiked]);
 
   return (
-    <div className={`like like--${isLiked}`} onClick={handleLike}>
+    <div className={`like like--${isLiked}`} onClick={() => { !isConnected() ? handleLike() : undefined }}>
       <Svg id="like" />
-      {showNumber && <p className="like__count">{count}</p>}
+      {showNumber || !isConnected() && <p className="like__count">{count}</p>}
     </div>
   );
 }
