127f55d67bbde8b130afcd18ad30edd48854c090
BeatlesBOB <beatlesbob74120@gmail.com>
2023-07-26 22:33:01 +0200

    fix: 🐛 fix focus

--- src/components/02 - Molecules/CodeEditor/CodeEditor.tsx
+++ src/components/02 - Molecules/CodeEditor/CodeEditor.tsx
@@ -1,12 +1,11 @@
 import { SandpackCodeEditor, useActiveCode } from "@codesandbox/sandpack-react";
 import { abbreviationTracker } from "@emmetio/codemirror6-plugin";
 import { searchKeymap, search } from "@codemirror/search";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Socket } from "socket.io-client";
 import { getDocument, peerExtension } from "@/utils/collab";
 import { generateName } from "@/utils/usernames";
 import { cursorExtension } from "@/utils/cursors";
-import { EditorSelection } from "@codemirror/state";
 
 import "./CodeEditor.scss";
 
@@ -14,10 +13,8 @@ export interface ICodeEditor {
   socket: Socket;
 }
 
-let editorKey = 0;
-
 export default function CodeEditor({ socket }: ICodeEditor) {
-  const { updateCode } = useActiveCode();
+  const { code, updateCode } = useActiveCode();
   const [isConnected, setIsConnected] = useState(false);
   const [version, setVersion] = useState(-1);
   const [documentName, setDocumentName] = useState("default-doc");
@@ -27,29 +24,26 @@ export default function CodeEditor({ socket }: ICodeEditor) {
   useEffect(() => {
     const fetchDoc = async () => {
       const { doc, version } = await getDocument(socket, documentName);
-      updateCode(doc.toString());
+      updateCode(doc.toString(), true);
       setVersion(version);
     };
-
     fetchDoc();
-
     socket.on("connect", () => {
       setIsConnected(true);
     });
-
     socket.on("disconnect", () => {
       setIsConnected(false);
     });
-
     socket.on("display", async (documentName) => {
       const { doc, version } = await getDocument(socket, documentName);
-      updateCode(doc.toString());
+      updateCode(doc.toString(), true);
       setVersion(version);
+      setDocumentName(documentName);
     });
-
     return () => {
       socket.off("connect");
       socket.off("disconnect");
+      socket.off("display");
       socket.off("pullUpdateResponse");
       socket.off("pushUpdateResponse");
       socket.off("getDocumentResponse");
@@ -57,23 +51,27 @@ export default function CodeEditor({ socket }: ICodeEditor) {
   }, []);
 
   return (
-    version != -1 && (
-      <SandpackCodeEditor
-        key={editorKey++}
-        showTabs
-        showLineNumbers
-        showInlineErrors
-        wrapContent
-        closableTabs
-        extensionsKeymap={[...searchKeymap]}
-        extensions={[
-          abbreviationTracker(),
-          search(),
-          peerExtension(socket, documentName, version, username),
-          cursorExtension(username)
-        ]}
-        className="editor__code"
-      />
+    true &&
+    useMemo(
+      () => (
+        <SandpackCodeEditor
+          key={crypto.randomUUID()}
+          showTabs
+          showLineNumbers
+          showInlineErrors
+          wrapContent
+          closableTabs
+          extensionsKeymap={[...searchKeymap]}
+          extensions={[
+            abbreviationTracker(),
+            search()
+            //peerExtension(socket, documentName, version, username),
+            //cursorExtension(username)
+          ]}
+          className="editor__code"
+        />
+      ),
+      []
     )
   );
 }
