957aae668a87a09775ee1988f7f996917e955966
Agence-de-Develoloppeurweb <123754014+Agence-de-Develoloppeurweb@users.noreply.github.com>
2023-08-03 19:26:01 +0200

    Update DesignSystem.tsx

--- src/pages/DesignSystem.tsx
+++ src/pages/DesignSystem.tsx
@@ -2,8 +2,8 @@ import Container from "@/components/01 - Atoms/Container/Container";
 import Editor from "@/components/03 - Organisms/Editor/Editor";
 export default function DesignSystem() {
   return (
-    <Container center={true}>
-      <Editor template="react" theme="dark" roomId="natho" />
-    </Container>
+    // <Container center={true}>
+    //   <Editor template="react" theme="dark" roomId="natho" />
+    // </Container>
   );
 }
