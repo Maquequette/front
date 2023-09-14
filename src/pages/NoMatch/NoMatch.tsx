26ee770c86a6e26582ef4d6933cf90cb17b38b04
BeatlesBOB <beatlesbob74120@gmail.com>
2023-09-14 20:59:07 +0200

    feat: 🎸 page nomatch

--- src/pages/NoMatch/NoMatch.tsx
+++ src/pages/NoMatch/NoMatch.tsx
@@ -1,3 +1,11 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+import Mac from "@/assets/images/Mac.json";
+import "./NoMatch.scss";
+
 export default function NoMatch(): JSX.Element {
-  return <div>NoMatch</div>;
+  return (
+    <div className="noMatch">
+      <Player src={Mac} autoplay loop />
+    </div>
+  );
 }
