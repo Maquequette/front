79b167f180b469d4881fc9e47ea4539ea292da76
BeatlesBOB <beatlesbob74120@gmail.com>
2023-09-14 22:41:52 +0200

    fix: 🐛 search img

--- src/pages/NoMatch/NoMatch.tsx
+++ src/pages/NoMatch/NoMatch.tsx
@@ -1,11 +1,15 @@
 import { Player } from "@lottiefiles/react-lottie-player";
+import PageTransition from "@/components/04 - Templates/PageTransition/PageTransition";
+
 import Mac from "@/assets/images/Mac.json";
 import "./NoMatch.scss";
 
 export default function NoMatch(): JSX.Element {
   return (
-    <div className="noMatch">
-      <Player src={Mac} autoplay loop />
-    </div>
+    <PageTransition>
+      <div className="noMatch">
+        <Player src={Mac} autoplay loop />
+      </div>
+    </PageTransition>
   );
 }
