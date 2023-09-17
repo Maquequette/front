5f83679d6db108b7e608964921bb7d5e190fbcb5
BeatlesBOB <beatlesbob74120@gmail.com>
2023-09-17 08:34:26 +0200

    feat: 🎸 init home

--- src/components/01 - Atoms/Image/Image.tsx
+++ src/components/01 - Atoms/Image/Image.tsx
@@ -1,4 +1,4 @@
-import { useState, memo } from "react";
+import { useState, memo, useMemo } from "react";
 import Dialog from "@/components/04 - Templates/Dialog/Dialog";
 import "./Image.scss";
 
@@ -12,9 +12,12 @@ export interface IImage {
 export default memo(function Image({ alt, src, height, width }: IImage) {
   const [isImageLoaded, setIsImageLoaded] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const color = useMemo(() => {
+    return ["primary", "secondary", "warn", "danger", "success", "accent"];
+  }, []);
 
   return (
-    <div className="img">
+    <div className="img__container">
       <img
         onClick={() => setIsOpen(true)}
         src={src}
@@ -24,7 +27,9 @@ export default memo(function Image({ alt, src, height, width }: IImage) {
         onLoad={() => {
           setIsImageLoaded(true);
         }}
-        className={`img ${!isImageLoaded ? "loading" : ""}`}
+        className={`img ${
+          !isImageLoaded ? `loading loading--${color.sort(() => 0.5 - Math.random())[0]}` : ""
+        }`}
         loading="lazy"
       />
       <Dialog visible={isOpen} id="modal__img" Dismiss={() => setIsOpen(false)}>
