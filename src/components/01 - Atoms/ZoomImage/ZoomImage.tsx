a35f354ab1d5c10eb6432b3dc6b770cd61c320b6
BeatlesBOB <beatlesbob74120@gmail.com>
2023-09-19 00:52:40 +0200

    Update ZoomImage.tsx

--- src/components/01 - Atoms/ZoomImage/ZoomImage.tsx
+++ src/components/01 - Atoms/ZoomImage/ZoomImage.tsx
@@ -68,29 +68,6 @@ const ZoomImage = ({ image }: { image: string }) => {
   };
 
   useEffect(() => {
-    observer.current = new ResizeObserver((entries) => {
-      entries.forEach(({ target }) => {
-        const { width, height } = background;
-        // If width of the container is smaller than image, scale image down
-        if (target.clientWidth < width) {
-          // Calculate scale
-          const scale = target.clientWidth / width;
-
-          // Redraw image
-          canvasRef.current.width = width * scale;
-          canvasRef.current.height = height * scale;
-          canvasRef.current
-            .getContext("2d")
-            .drawImage(background, 0, 0, width * scale, height * scale);
-        }
-      });
-    });
-    observer.current.observe(containerRef.current);
-
-    return () => observer.current.unobserve(containerRef.current);
-  }, []);
-
-  useEffect(() => {
     background.src = image;
 
     if (canvasRef.current) {
