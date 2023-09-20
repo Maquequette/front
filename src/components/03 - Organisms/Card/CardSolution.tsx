920fcabc2ee3e803d9a78c453947851f433cf968
Agence-de-Develoloppeurweb <develoloppeurweb@gmail.com>
2023-09-21 01:57:01 +0200

    fix merge

--- src/components/03 - Organisms/Card/CardSolution.tsx
+++ src/components/03 - Organisms/Card/CardSolution.tsx
@@ -9,48 +9,38 @@ import Svg from "@/components/01 - Atoms/Svg/Svg";
 import "./CardSolution.scss";
 
 export interface ICardSolution {
-    author: any;
-    date: number | Date;
-    id: number;
-    recap: string;
-    onclick: Function;
-    isLiked?: boolean;
-    likesCount?: number;
-    commentsCount?: number;
+  author: any;
+  date: number | Date;
+  id: number;
+  recap: string;
+  onclick: Function;
+  isLiked?: boolean;
+  likesCount?: number;
+  commentsCount?: number;
 }
 
 export default memo(function CardSolution({ id, recap, author, date, onclick, isLiked, commentsCount }: ICardSolution) {
 
-    return (
-        <div className="cardSolution" onClick={(event) => {
-            event.stopPropagation();
-            onclick();
-        }}>
-            <div className="cardSolution__header">
-                <Date date={date} />
-                <BulletPoint />
-                <Like showNumber={true} id={id} isAlreadyLiked={isLiked} type="Solution" />
-                <BulletPoint />
-                <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', gap: '1rem' }}>
-                    <Svg id="comment" styles={{ width: "2rem", height: "2rem" }} />
-                    <p className="comment__count">{commentsCount}</p>
-                </div>
-            </div>
-            <div className="cardSolution__profile">
-                <ProfileButton id={author.id} username={`${author.firstName} ${author.lastName}`} />
-            </div>
-            <div className="cardSolution__content">
-                <Paragraph color="dark" isHtml={true} styles={{ paddingBottom: '1rem' }}>
-                    {DOMPurify.sanitize(recap ?? "")}
-                </Paragraph>
-            </div>
+  return (
+    <div className="cardSolution" onClick={(event) => {
+      event.stopPropagation();
+      onclick();
+    }}>
+      <div className="cardSolution__header">
+        <Date date={date} />
+        <BulletPoint />
+        <Like showNumber={true} id={id} isAlreadyLiked={isLiked} type="Solution" />
+        <BulletPoint />
+        <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', gap: '1rem' }}>
+          <Svg id="comment" styles={{ width: "2rem", height: "2rem" }} />
+          <p className="comment__count">{commentsCount}</p>
         </div>
       </div>
       <div className="cardSolution__profile">
         <ProfileButton id={author.id} username={`${author.firstName} ${author.lastName}`} />
       </div>
       <div className="cardSolution__content">
-        <Paragraph color="dark" isHtml={true} styles={{ paddingBottom: "1rem" }}>
+        <Paragraph color="dark" isHtml={true} styles={{ paddingBottom: '1rem' }}>
           {DOMPurify.sanitize(recap ?? "")}
         </Paragraph>
       </div>
